266376f76552f6916da4093c10be81c96b31ca32
chenjiong <1453538137@qq.com>
2020-04-10 11:21:48 +0800

    修改过后的新增的测试实体类

--- src/main/java/com/gonmao/bean/Student.java
+++ src/main/java/com/gonmao/bean/Student.java
@@ -9,4 +9,27 @@ public class Student {
     private int stuId;
     private String stuName;
 
+    public int getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(int stuId) {
+        this.stuId = stuId;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "stuId=" + stuId +
+                ", stuName='" + stuName + '\'' +
+                '}';
+    }
 }
